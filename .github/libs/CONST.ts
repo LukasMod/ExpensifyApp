9361d8e85436fd103018626433fae24fb5659906
Lukasz Modzelewski <lukasz.modzelewski@callstack.com>
2025-09-12 18:41:39 +0200

    test

--- .github/libs/CONST.ts
+++ .github/libs/CONST.ts
@@ -19,7 +19,7 @@ const CONST = {
         CP_STAGING: 'CP Staging',
     },
     STATE: {
-        OPEN: 'open', // test //test22
+        OPEN: 'open', // test //test222
     },
     COMMENT: {
         TYPE_BOT: 'Bot',
