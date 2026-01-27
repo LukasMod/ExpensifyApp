c1b84d42d67e2788c54c3b02b84bb46aebe34b7c
Lukasz Modzelewski <lukasz.modzelewski@callstack.com>
2026-01-27 09:49:07 +0100

    test1

--- src/Expensify.tsx
+++ src/Expensify.tsx
@@ -121,7 +121,7 @@ function Expensify() {
     const [stashedCredentials = CONST.EMPTY_OBJECT] = useOnyx(ONYXKEYS.STASHED_CREDENTIALS, {canBeMissing: true});
     const [stashedSession] = useOnyx(ONYXKEYS.STASHED_SESSION, {canBeMissing: true});
 
-    console.log('TEST 8', )
+    console.log('TEST 1', )
 
     useDebugShortcut();
     usePriorityMode();
