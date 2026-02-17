cfa890a01c19635e4e0776cfbe91a22ed4427743
Lukasz Modzelewski <lukasz.modzelewski@callstack.com>
2026-02-17 11:37:36 +0100

    PR #49: Add auto-generated comment to Expensify.tsx
    This is an auto-generated change for testing purposes.
    Created at: 2026-02-17 11:37:36

--- src/Expensify.tsx
+++ src/Expensify.tsx
@@ -1,3 +1,4 @@
+// PR #49 - Auto-generated change (2026-02-17 11:37:36)
 // PR PR-4744 - Auto-generated change (2026-02-17 10:28:09)
 import HybridAppModule from '@expensify/react-native-hybrid-app';
 import * as Sentry from '@sentry/react-native';
