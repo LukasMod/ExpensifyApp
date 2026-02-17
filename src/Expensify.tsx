3c9c2665e431b12c2d7f659156fb8df98d486ca2
Lukasz Modzelewski <lukasz.modzelewski@callstack.com>
2026-02-17 10:06:23 +0100

    PR PR-2878: Add auto-generated comment to Expensify.tsx
    This is an auto-generated change for testing purposes.
    Created at: 2026-02-17 10:06:23

--- src/Expensify.tsx
+++ src/Expensify.tsx
@@ -1,3 +1,4 @@
+// PR PR-2878 - Auto-generated change (2026-02-17 10:06:23)
 import HybridAppModule from '@expensify/react-native-hybrid-app';
 import * as Sentry from '@sentry/react-native';
 import React, {useCallback, useEffect, useLayoutEffect, useRef, useState} from 'react';
